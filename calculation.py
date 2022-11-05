324ca67bb45af34368377e68bb61df57304dd5bd
karuna-bhalerao <karunabhalerao786@gmail.com>
2022-11-05 15:12:09 +0530

    added code for add and sub

---
+++ calculation.py
@@ -0,0 +1,10 @@
+def addition (a,b):
+    c=a+b
+    return c
+def substraction(a,b):
+    c= a-b
+    return c
+a=30
+b=15
+print(f"addition of {a} and {b} is {addition(a,b)}")
+print(f"substraction of {a} and {b} is {substraction(a,b)}")
